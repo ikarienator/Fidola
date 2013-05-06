aa0f7c0937531952c0549880924687bd96903987
ZHANG Bei <ikarienator@gmail.com>
2013-05-06 00:02:25 -0700

    Fix fntt

--- test/spec/NT.spec.js
+++ test/spec/NT.spec.js
@@ -35,34 +35,16 @@ describe("Number Theory Algorithm", function () {
             expect(fast.nt.primeQ(4)).to.eql(false);
             expect(fast.nt.primeQ(761920934)).to.eql(false);
         });
-        it("117 is not prime", function () {
+        it("Non-trivial cases", function () {
             expect(fast.nt.primeQ(117)).to.eql(false);
-        });
-        it("119 is not prime", function () {
             expect(fast.nt.primeQ(117)).to.eql(false);
-        });
-        it("1048571 is prime", function () {
             expect(fast.nt.primeQ(1048571)).to.eql(true);
-        });
-        it("1048573 is prime", function () {
             expect(fast.nt.primeQ(1048573)).to.eql(true);
-        });
-        it("1048577 is not prime", function () {
             expect(fast.nt.primeQ(1048577)).to.eql(false);
-        });
-        it("142523471 is prime", function () {
             expect(fast.nt.primeQ(142523471)).to.eql(true);
-        });
-        it("142523473 is not prime", function () {
             expect(fast.nt.primeQ(142523473)).to.eql(false);
-        });
-        it("142523479 is not prime", function () {
             expect(fast.nt.primeQ(142523479)).to.eql(false);
-        });
-        it("761920933 is prime", function () {
             expect(fast.nt.primeQ(761920933)).to.eql(true);
-        });
-        it("761920913 is not prime", function () {
             expect(fast.nt.primeQ(761920913)).to.eql(false);
         })
     });
